a99d2402361414b912b34e6eca85d4af6610d8b8
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2024-10-04 23:38:30 +0900

    [level 3] Title: 입국심사, Time: 176.36 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,12 +1,10 @@
 function solution(n, times) {
-    let left = 1; // 최소 1분부터
-    let right = Math.max(...times) * n; // 최대 (제일 느린 심사관이 모든 심사를 처리하는 경우)
+    let [left, right] = [1, Math.max(...times) * n];
     
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
-        const sum = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0); // 심사에 걸리는 평균 시간
+        const sum = times.reduce((acc, cur) => acc + Math.floor(mid / cur), 0);
         
-        // 입국자는 n명이므로, 그 이상은 처리할 필요가 없다
         if (sum < n) left = mid + 1;
         else right = mid - 1;
     }
