a97c8e87109fadacc15df034a77eaa980aeadaf1
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2024-10-13 23:58:25 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,7 @@
+function solution(price) {
+    if (price >= 100000 && price < 300000) price -= price * 0.05;
+    else if (price >= 300000 && price < 500000) price -= price * 0.1;
+    else if (price >= 500000) price -= price * 0.2;
+    
+    return Math.floor(price);
+}
