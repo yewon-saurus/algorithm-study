3815d1d39d0da214f38e8a9083c2850206101e9a
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2024-10-14 17:43:28 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return [...my_string].filter(item => item == Number(item)).map(item => Number(item)).reduce((acc, cur) => acc += cur, 0);
+    return [...my_string].filter(item => item == Number(item)).reduce((acc, cur) => acc += Number(cur), 0);
 }
