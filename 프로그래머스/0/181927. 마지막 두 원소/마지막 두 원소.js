c68c7a4cd2d2504384782473fba7bc1328fa2087
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2024-12-20 19:06:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,3 @@
+function solution(num_list) {
+    return [...num_list, num_list[num_list.length - 1] > num_list[num_list.length - 2] ? num_list[num_list.length - 1] - num_list[num_list.length - 2] : num_list[num_list.length - 1] * 2];
+}
