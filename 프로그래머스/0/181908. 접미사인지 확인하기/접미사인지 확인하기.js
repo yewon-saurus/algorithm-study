275c3aa9470d51d51de1d2d2a2892c1ac4e8dae5
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-01-13 13:51:18 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,5 +1,3 @@
 function solution(my_string, is_suffix) {
-    return my_string.length - is_suffix.length >= 0
-        ? +(my_string.lastIndexOf(is_suffix) === my_string.length - is_suffix.length)
-        : 0;
+    return my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0;
 }
