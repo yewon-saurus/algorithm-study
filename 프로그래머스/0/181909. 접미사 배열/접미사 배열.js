5618ffca260dc8612f07a4bff43768c1ae7c19da
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-01-14 21:06:37 +0900

    [level 0] Title: 접미사 배열, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    const answer = [];
+    
+    for (let i = my_string.length - 1; i >= 0; i--) {
+        answer.push(my_string.slice(-i));
+    }
+    answer.sort();
+    
+    return answer;
+}
