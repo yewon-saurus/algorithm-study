5f4962d2a249ca84544827afe2297a2d2005bb79
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-01-14 13:44:17 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,12 +1,7 @@
-function solution(n) {
-    const answer = [];
-    
-    while (n !== 1) {
-        answer.push(n);
-        if (n % 2 === 0) n /= 2;
-        else n = 3 * n + 1;
-    }
-    answer.push(n);
-    
-    return answer;
+function solution(n, arr=[]) {
+    // 재귀 풀이
+    arr.push(n);
+    if (n === 1) return arr;
+    if (n % 2 === 0) return solution(n / 2, arr);
+    else return solution(3 * n + 1, arr);
 }
