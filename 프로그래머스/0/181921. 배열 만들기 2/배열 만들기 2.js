6ee4869c6550e06f93c1a57709aeb725b9e4ac2d
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-01-15 21:39:08 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,19 @@
+function solution(l, r) {
+    let tree = [5]; // 5는 root
+    
+    // index
+    let cur, left, right;
+    
+    const needVisit = [0];
+    while (tree[tree.length - 1] < r) {
+        cur = needVisit.shift();
+        left = cur * 2 + 1;
+        right = cur * 2 + 2;
+        needVisit.push(left, right);
+        
+        tree[left] = tree[cur] * 10;
+        tree[right] = tree[cur] * 10 + 5;
+    }
+    
+    return tree.filter(ele => ele >= l && ele <= r).length === 0 ? [-1] : tree.filter(ele => ele >= l && ele <= r);
+}
