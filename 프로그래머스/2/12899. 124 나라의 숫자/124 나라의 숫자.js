bbbc52df2f62bcec63a692ea2fc109fe157c0d53
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-04-08 16:04:45 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.22 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,19 @@
+function solution(n) {
+    return convertTo124(n);
+}
+
+function convertTo124(n) {
+    let result = [];
+    const convertTable = [4, 1, 2];
+    // 나머지 계산 결과(배열의 index 값에 대응)에 따른 result 출력 값 매칭
+    // 3을 3으로 나누었을 때 나머지가 0 -> 4로 변환되어야 함
+    // 1을 3으로 나누었을 때 나머지가 1
+    // 2를 3로 나누었을 때 나머지가 2
+    
+    while (n > 0) {
+        result = [convertTable[n % 3], ...result];
+        n = Math.floor((n - 1) / 3);
+    }
+    
+    return result.join('');
+}
