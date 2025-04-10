ce20c9b9010e7c1413fa17b74310e09bd24c2643
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-04-10 20:15:05 +0900

    [level 2] Title: 2 x n 타일링, Time: 4.27 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const dp = new Array(n+1).fill(0);
+    dp[0] = 1;
+    dp[1] = 1;
+  
+    for(let i = 2; i <= n; i++) {
+        dp[i] = (dp[i-2] + dp[i-1]) > 1000000007 ? (dp[i-2] + dp[i-1]) % 1000000007 : (dp[i-2] + dp[i-1]);
+    }
+
+    return dp[n];
+}
