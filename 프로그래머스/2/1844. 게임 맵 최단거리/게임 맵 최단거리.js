d0042fdcb99c1fee2d6cadf9be32a7c086acc956
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-04-21 17:26:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 20.66 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -1,11 +1,11 @@
 const delta = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
 function solution(maps) {
-    let queue = [];
-    queue.push([0, 0, 1]); // x, y, distance
+    let queue = new Queue();
+    queue.enqueue([0, 0, 1]); // x, y, distance
     
-    while (queue.length > 0) {
-        const [x, y, distance] = queue.shift();
+    while (!queue.isEmpty()) {
+        const [x, y, distance] = queue.dequeue();
         
         for (const [dx, dy] of delta) {
             if (
@@ -14,10 +14,33 @@ function solution(maps) {
                 maps[x + dx][y + dy] === 1
             ) {
                 maps[x + dx][y + dy] = distance + 1;
-                queue.push([x + dx, y + dy, maps[x + dx][y + dy]]);
+                queue.enqueue([x + dx, y + dy, maps[x + dx][y + dy]]);
             }
         }
     }
     
     return maps[maps.length - 1][maps[0].length - 1] <= 1 ? -1 : maps[maps.length - 1][maps[0].length - 1];
+}
+
+class Queue {
+    constructor() {
+        this.queue = [];
+        this.head = 0;
+        this.tail = 0;
+    }
+    
+    enqueue(value) {
+        this.queue.push(value);
+        this.tail++;
+    }
+    
+    dequeue() {
+        const returnValue = this.queue[this.head];
+        this.head++;
+        return returnValue;
+    }
+    
+    isEmpty() {
+        return this.tail - this.head === 0;
+    }
 }
