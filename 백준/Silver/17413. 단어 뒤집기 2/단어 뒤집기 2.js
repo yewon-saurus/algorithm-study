2e99b3b16877663239b7f0b7bffbeb49d5ed2ed3
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-09-03 19:07:24 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 108 ms, Memory: 14576 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.js
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.js
@@ -2,45 +2,10 @@ const fs = require('fs');
 const filePath = fs.existsSync('./test.txt') ? './test.txt' : '/dev/stdin';
 let input = fs.readFileSync(filePath).toString().trim();
 
-let stack = [];
-let answer = [];
-
-for (const c of input) {
-    stack.push(c);
-    if (c === " ") {
-        if (stack[0] !== "<") {
-            stack.pop();
-            answer.push([0, stack.join("")]);
-            stack = [];
-        }
-    } else if (c === "<") {
-        if (stack.length > 1) {
-            stack.pop();
-            answer.push([0, stack.join("")]);
-            stack = ["<"];
-        }
-    } else if (c === ">") {
-        answer.push([1, stack.join("")]);
-        stack = [];
-    }
-}
-
-if (stack.length > 0) {
-    answer.push([0, stack.join("")]);
-}
+let regExp = /<[a-z\s]+>|[a-z0-9]+/g;
 
 console.log(
-    answer
-        .map((ele, idx, origin) => {
-            if (ele[0]) return ele[1];
-            else {
-                if (idx + 1 < origin.length && origin[idx + 1][0]) {
-                    return ele[1].split("").reverse().join("");
-                }
-                else {
-                    return ele[1].split("").reverse().join("") + " ";
-                }
-            }
-        })
-        .join("")
+    input.replace(regExp, (word) => {
+        return word.startsWith("<") ? word : word.split("").reverse().join("");
+    })
 );
