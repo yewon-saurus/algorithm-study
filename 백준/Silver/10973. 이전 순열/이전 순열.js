1a271ebe1cba22ce03d16985098467d4a861961b
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-09-05 20:12:25 +0900

    [Silver III] Title: 이전 순열, Time: 104 ms, Memory: 11168 KB -BaekjoonHub

--- 백준/Silver/10973. 이전 순열/이전 순열.js
+++ 백준/Silver/10973. 이전 순열/이전 순열.js
@@ -9,19 +9,14 @@ function prevPermutation(arr) {
   let i = arr.length - 1;
   while (i > 0 && arr[i - 1] <= arr[i]) i--;
 
-  if (i <= 0) return false; // 이미 가장 작은 순열 -> 이전 없음
+  if (i <= 0) return false;
 
   let j = arr.length - 1;
   while (arr[i - 1] <= arr[j]) j--;
 
   [arr[i - 1], arr[j]] = [arr[j], arr[i - 1]];
 
-  // i부터 끝까지 뒤집기
-  let left = i, right = arr.length - 1;
-  while (left < right) {
-    [arr[left], arr[right]] = [arr[right], arr[left]];
-    left++; right--;
-  }
+  arr.splice(i, arr.length - i, ...arr.slice(i).reverse());
 
   return true;
 }
