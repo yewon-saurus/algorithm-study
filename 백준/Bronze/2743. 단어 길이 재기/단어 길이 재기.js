c0875c4fb253755ff219c09c71c6f87e18034868
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-09-24 15:57:18 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath = fs.existsSync('./test.txt') ? './test.txt' : '/dev/stdin';
+let input = fs.readFileSync(filePath).toString().trim();
+
+console.log(input.length);
