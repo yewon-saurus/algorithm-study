61e384f1daa2cc2563b85eb74a548aa3cd230596
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-10-05 20:49:42 +0900

    [Bronze I] Title: 달팽이2, Time: 172 ms, Memory: 11960 KB -BaekjoonHub

---
+++ 백준/Bronze/1952. 달팽이2/달팽이2.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const filePath = fs.existsSync('./test.txt') ? './test.txt' : '/dev/stdin';
+let [M, N] = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+let answer = 0;
+let table = Array.from({ length: M }, (_) => new Array(N).fill(0));
+let [y, x] = [0, 0];
+table[y][x] = 1;
+
+const DIR = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+while (true) {
+    const [dy, dx] = DIR[answer % 4];
+    let [ny, nx] = [y + dy, x + dx];
+    
+    if (
+        ny < 0 || ny >= M ||
+        nx < 0 || nx >= N ||
+        table[ny][nx] !== 0
+    ) {
+        answer += 1;
+
+        const [tdy, tdx] = DIR[answer % 4];
+        const ty = y + tdy;
+        const tx = x + tdx;
+
+        if (ty < 0 || ty >= M || tx < 0 || tx >= N || table[ty][tx] !== 0) {
+            answer -= 1;
+            break;
+        }
+        continue;
+    }
+
+    y = ny;
+    x = nx;
+    table[y][x] = 1;
+}
+
+console.log(answer);
