b9d7cb57bba52bd7736847bea788e40ad1a28a83
Yewon Kim <53112143+yewon-saurus@users.noreply.github.com>
2025-11-08 12:42:20 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.py
@@ -1,8 +1,4 @@
 def solution(array, height):
-    answer = 0
-    
-    for i in array:
-        if i > height:
-            answer += 1
-    
-    return answer
+    array.append(height)
+    array.sort(reverse=True)
+    return array.index(height)
